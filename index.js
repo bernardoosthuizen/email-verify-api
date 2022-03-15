f51f767f0ec09e2e440341b1f830eb401e7f7754
Bernard Oosthuizen <bernard.oosthuizen@gmail.com>
2022-03-15 09:48:27 +0800

    outline / notes added

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Receive email via JSON
+
+// Verify it is an email address
+
+// Send a verification email to user
+    // User click on link to endpoint that displays a confirmation page
+    // Endpoint also tells server it is verified
+
+// Once verified send response to origin
+
